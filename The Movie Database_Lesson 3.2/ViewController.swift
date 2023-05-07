1d76cfcba0d39f73d8e0a63cbfc51c40bec38054
Aleksz <aleksznoob@gmail.com>
2023-05-07 14:56:41 +0200

    Initial Commit

---
+++ The Movie Database_Lesson 3.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie Database_Lesson 3.2
+//
+//  Created by Иван on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
